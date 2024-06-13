787c95698254d974b9e64ed952542657a3f7a798
Raúl <rulokoji@gmail.com>
2024-06-13 23:48:42 +0200

    colored urgencies

--- main.cpp
+++ main.cpp
@@ -19,16 +19,16 @@ std::vector<Task> load_tasks() {
         std::string line;
         while (std::getline(file, line)) {
             size_t pos = line.find('|');
-	    std::cout << pos << std::endl;
+	    //std::cout << pos << std::endl;
             if (pos != std::string::npos) {
                 int id = std::stoi(line.substr(0, pos));
 		line = line.substr(pos + 1);
 		pos = line.find('|');
-	        std::cout << pos << std::endl;
+	        //std::cout << pos << std::endl;
 		if(pos != std::string::npos){
                     std::string description = line.substr(0, pos);
 		    int urgency = std::stoi(line.substr(pos + 1));
-		    std::cout << id << " " << description << " " << urgency << std::endl;  
+		    //std::cout << id << " " << description << " " << urgency << std::endl;  
                     tasks.push_back({id, description, urgency});
             	}
 	    }
@@ -49,18 +49,44 @@ void save_tasks(const std::vector<Task>& tasks) {
     }
 }
 
+void taskColor(std::string & colorInit, std::string & colorEnd, const int &  urgency){
+    switch(urgency){
+   
+  	case 3:
+	    colorInit = "\033[33m";
+	    colorEnd = "\033[0m";
+	    break;	
+	    
+	case 4:
+	    colorInit = "\033[31m";
+	    colorEnd = "\033[0m";
+	    break;	
+	    	
+	case 5:	
+	    colorInit = "\033[1;31m";
+	    colorEnd = "\033[0m";
+	    break;	    
+    }
+}
+
 void list_tasks() {
+    
+    std::string colorInit = "";
+    std::string colorEnd = "";
     std::vector<Task> tasks = load_tasks();
+    
     if (tasks.empty()) {
        //std::cout << "No tasks available.\n";
     } else {
         for (const auto& task : tasks) {
-            std::cout << "ID: " << task.id << ", Description: " << task.description << ", Urgency: " << task.urgency << "\n";
+	    int urg = task.urgency;
+	    taskColor(colorInit, colorEnd, urg);
+            std::cout << colorInit << "ID: " << task.id << ", Description: " << task.description << ", Urgency: " << urg << colorEnd << "\n";
         }
     }
 }
 
-void add_desc(const std::string& description, int& urgency) {
+void add_task(const std::string& description, int& urgency) {
     std::vector<Task> tasks = load_tasks();
     int new_id = tasks.empty() ? 1 : tasks.back().id + 1;
     tasks.push_back({new_id, description, urgency});
@@ -87,37 +113,27 @@ void remove_task(int id) {
 int main(int argc, char** argv) {
     CLI::App app{"Personal Task Manager"};
 
-    std::string description;
-    int id = -1;
+    std::string description = "";
     bool list = false;
-    bool add = false;
-    bool remove = false;
+    int id = -1;
     int urgency = 1;
 
     app.add_flag("-l,--list", list, "List all tasks");
-    app.add_flag("-a,--add", add, "Add a new task");
-    app.add_flag("-c,--check", remove, "Check the task with the given ID");
-    app.add_option("-d,--description", description, "Description of the new task")->needs("-a");
-    app.add_option("-i,--id", id, "ID of the task to remove")->needs("-r");
-    app.add_option("-u,--urgency", urgency, "Urgency of the task 1-5")->needs("-d");
+    app.add_option("-a,--add", description, "Add a new task");
+    app.add_option("-c,--check", id, "Check the task with the given ID");
+    app.add_option("-u,--urgency", urgency, "Urgency of the task 1-5")->needs("-a");
     //std::cout << "VALID ARGUMENTS" << std::endl;
     CLI11_PARSE(app, argc, argv);
 
     if (list) {
         list_tasks();
-    } else if (add) {
-        if (!description.empty()) {
-            add_desc(description, urgency);
-        } else {
-            std::cerr << "Task description cannot be empty.\n";
+    } else if (!description.empty()) {
+        add_task(description, urgency);
         }
-    } else if (remove) {
-        if (id != -1) {
+      else if (id  != -1) {
             remove_task(id);
-        } else {
-            std::cerr << "Task ID is required to remove a task.\n";
         }
-    } else {
+      else {
 	    std::cout << app.help() << std::endl;
     }
 
